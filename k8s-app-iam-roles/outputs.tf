2c4fd3be4f91ce7f006b303739b419388a194c52
Neil McGibbon <code@neilmcgibbon.com>
2024-03-05 16:57:40 +0000

    chore: fix outputs

--- k8s-app-iam-roles/outputs.tf
+++ k8s-app-iam-roles/outputs.tf
@@ -1,14 +1,14 @@
 output "aws_load_balancer_controller_role_arn" {
   description = "IAM role ARN for aws-load-balancer-controller"
-  value       = module.aws_load_balancer_controller.role_arn
+  value       = var.aws_load_balancer_controller.enabled ? module.aws_load_balancer_controller[0].role_arn : "not-enabled"
 }
 
 output "external_dns_role_arn" {
   description = "IAM role ARN for external-dns"
-  value       = module.external_dns.role_arn
+  value       = var.external_dns.enabled ? module.external_dns[0].role_arn : "not-enabled"
 }
 
 output "external_secrets_role_arn" {
   description = "IAM role ARN for external-secrets"
-  value       = module.external_secrets.role_arn
+  value       = var.external_secrets.enabled ? module.external_secrets[0].role_arn : "not-enabled"
 }
