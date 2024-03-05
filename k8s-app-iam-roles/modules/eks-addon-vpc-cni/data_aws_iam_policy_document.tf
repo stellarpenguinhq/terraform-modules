db091310bbd8a88154af3325b5e52044de96903a
Neil McGibbon <code@neilmcgibbon.com>
2024-03-05 18:29:29 +0000

    fix(k8s-app-iam-roles): vpc-cnu trust

--- k8s-app-iam-roles/modules/eks-addon-vpc-cni/data_aws_iam_policy_document.tf
+++ k8s-app-iam-roles/modules/eks-addon-vpc-cni/data_aws_iam_policy_document.tf
@@ -16,12 +16,12 @@ data "aws_iam_policy_document" "trust" {
 
     condition {
       test     = "StringEquals"
-      variable = "${substr(var.eks_cluster_oidc_provider_arn, 8, -1)}:aud"
+      variable = "${substr(var.eks_cluster_oidc_issuer_url, 8, -1)}:aud"
       values   = ["sts.amazonaws.com"]
     }
 
     condition {
-      test     = "StringLike"
+      test     = "StringEquals"
       variable = "${substr(var.eks_cluster_oidc_issuer_url, 8, -1)}:sub"
       values   = ["system:serviceaccount:${var.kubernetes_namespace}:${var.kubernetes_service_acount_name}"]
     }
