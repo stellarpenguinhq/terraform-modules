3f209a43e758c85674ab86dc697d3405482b25c7
Neil McGibbon <code@neilmcgibbon.com>
2026-04-21 08:32:12 +0100

    chore: pod identity roles for EKS addons

--- k8s-app-pod-identity-roles/aws-efs-csi/mod_eks_role.tf
+++ k8s-app-pod-identity-roles/aws-efs-csi/mod_eks_role.tf
@@ -4,12 +4,33 @@
 #
 ###############################################################################
 module "eks_role" {
-  source                          = "git@github.com:stellarpenguinhq/terraform-modules.git//eks-role?ref=eks-role-v1.0.0"
-  cluster_name                    = var.cluster_name
-  role_name                       = var.role_name
-  service_account_namespace       = var.service_account_namespace
-  service_account_name            = var.service_account_name
-  managed_policies                = ["arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"]
+  source                    = "git@github.com:stellarpenguinhq/terraform-modules.git//eks-role?ref=eks-role-v1.0.0"
+  cluster_name              = var.cluster_name
+  role_name                 = var.role_name
+  service_account_namespace = var.service_account_namespace
+  service_account_name      = var.service_account_name
+  managed_policies = [
+    "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy",
+    "arn:aws:iam::aws:policy/service-role/AmazonS3FilesCSIDriverPolicy",
+  ]
+  create_pod_identity_association = false
+}
+
+###############################################################################
+#
+# EKS Role: EFS CSI (EKS AddOn)
+#
+###############################################################################
+module "eks_role" {
+  source                    = "git@github.com:stellarpenguinhq/terraform-modules.git//eks-role?ref=eks-role-v1.0.0"
+  cluster_name              = var.cluster_name
+  role_name                 = "${var.role_name}-node"
+  service_account_namespace = var.service_account_namespace
+  service_account_name      = var.service_account_name
+  managed_policies = [
+    "arn:aws:iam::aws:policy/AmazonElasticFileSystemsUtils",
+    "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess",
+  ]
   create_pod_identity_association = false
 }
 
